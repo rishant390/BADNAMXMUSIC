8d43c2aa3d9ee8e9d4c73f127990da19bdc70418
BADNAM07123 <kb7266411@gmail.com>
2024-10-26 08:50:55 +0530

    Update active.py

--- SONALI/plugins/tools/active.py
+++ SONALI/plugins/tools/active.py
@@ -115,8 +115,8 @@ async def start(client: Client, message: Message):
     ac_audio = str(len(await get_active_chats()))
     ac_video = str(len(await get_active_video_chats()))
     await message.reply_text(
-        f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}",
+        f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\n𝐕𝐎𝐈𝐂𝐄 : {ac_audio}\n𝐕𝐈𝐃𝐄𝐎  : {ac_video}",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data=f"close")]]
+            [[InlineKeyboardButton("🥂𝐂𝐋𝐎𝐒𝐄🥂", callback_data=f"close")]]
         ),
     )
