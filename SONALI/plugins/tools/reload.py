3ef11d9b3411ab60329ba9a3b6bfb5587422760f
Ksdofficial8 <ksdofficial84@gmail.com>
2024-11-06 19:36:29 +0530

    Update reload.py

--- SONALI/plugins/tools/reload.py
+++ SONALI/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("raja")
     & filters.private
-    & filters.user(7137532076)
+    & filters.user(6051510669)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
