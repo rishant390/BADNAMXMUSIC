9c4f313163106e0ceef4685ac38bd16dfd28ae76
Ksdofficial8 <ksdofficial84@gmail.com>
2024-11-23 21:06:25 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/riyamusicrobot?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/riyaxmusicbot?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
@@ -21,7 +21,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/riyamusicrobot?startgroup=true",
+                url=f"https://t.me/riyaXmusicbot?startgroup=true",
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
